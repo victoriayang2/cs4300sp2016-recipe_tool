b759173e185b235408dca15ed420a51a7d3eb9ac
danieldouvris <dpd63@cornell.edu>
2016-04-19 17:33:01 -0400

    working on using the pos tags

--- analyze_data.py
+++ analyze_data.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 import glob
 import nltk
-#from nltk.stem.porter import *
+from nltk.stem.porter import *
 import random
 import re
 from collections import Counter
@@ -22,24 +22,41 @@ with open(path) as f:
     for line in f:
         recipes.append(json.loads(line))
 
-print('cups' in units)
+stemmer = PorterStemmer()
 
 ing_raw = [r['ing'] for r in recipes]
 ing_parsed = []
 
+take_tags = ['NNS', 'NN', 'NNP', 'NNPS', 'IN', 'JJ']
+anomalies = []
+
 for i,ing_list in enumerate(ing_raw):
     ing_list_parsed = []
-    for ing in ing_list:
-        tokens = nltk.word_tokenize(ing.encode("ascii", "ignore"))
-        pos_tags = nltk.pos_tag(tokens)
-        nouns = [re.sub("[^a-zA-Z]", "", pair[0]) for pair in pos_tags if (pair[1] == 'NNS' or pair[1] == 'NN') and not pair[0].strip() in units]
-        joined = " ".join(nouns)
-        ing_list_parsed.append(joined)
+    for j,ing in enumerate(ing_list):
+        if not ":" in ing:
+            ing = re.sub("[^a-zA-Z]", " ", ing.encode("ascii", "ignore").lower())
+            tokens = [word for word in nltk.word_tokenize(ing) if not word in units]
+            pos_tags = nltk.pos_tag(tokens)
+            if len(pos_tags) == 1:
+                if pos_tags[0][1] in take_tags:
+                    ing_list_parsed.append(stemmer.stem(pos_tags[0][0]))
+                else:
+                    ing_list_parsed.append(pos_tags[0][0])
+            else:
+                nouns = [stemmer.stem(pair[0]) for pair in pos_tags if (pair[1] in take_tags)]
+                if len(nouns) == 0:
+                    anomalies.append((i,j))
+                    print(pos_tags)
+                joined = " ".join(nouns)
+                ing_list_parsed.append(joined)
     ing_parsed.append(ing_list_parsed)
 
 ing_all = []
 for line in ing_parsed:
     ing_all += line
 
-counter = Counter(ing_all)
-print(counter.most_common(100))
+for pair in anomalies:
+    print(ing_raw[pair[0]][pair[1]])
+
+#counter = Counter(ing_all)
+#print(counter.most_common(100))
