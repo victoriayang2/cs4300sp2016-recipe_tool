17ab900d1ab1f24ff648ccf36cee9997bd62458a
Prava Samad <pts54@cornell.edu>
2016-04-22 01:43:49 -0400

    Search Result Logic (real)
    Didn't upload the updated file last time. whoops.

--- project_template/test.py
+++ project_template/test.py
@@ -10,23 +10,61 @@ def read_file(n):
 	transcripts = json.load(file)
 	return transcripts
 
+# Some of the recipe names contain non-unicode characters which throw errors 
+# so must be checked before applying edit distance
 def _edit(query, msg):
-    return Levenshtein.distance(query.lower(), msg.lower())
+    try:
+        query = str(query).lower()
+    except UnicodeEncodeError:
+        query = (query.encode('utf8')).lower()
+    
+    try:
+        msg = str(msg).lower()
+    except UnicodeEncodeError:
+        msg = (msg.encode('utf8')).lower()
+        
+    return Levenshtein.distance(query, msg)
 
+# Returns list of tuples (edit_distance,recipe) ordered by edit distance
 def find_similar(q):
-	transcripts = read_file(1)
-	result = []
-	for transcript in transcripts:
-		for item in transcript:
-			m = item['text']
-			result.append(((_edit(q, m)), m))
-
-	return sorted(result, key=lambda tup: tup[0])
+    transcripts = read_file(1)
+    result = []
+    for transcript in transcripts:
+        m = transcript['name']
+        result.append(((_edit(q, m)), transcript))
+    return sorted(result, key=lambda tup: tup[0])
 
 def find_recipes(i,r):
-	#split i and r based on , 
-	# compute the similarity on names using edit distance
-	# compute the resultant of recipes based on intersection of output of recipes from inverted index
-	# use both or have different modules to see which one is better
-	# return the resultant recipe list json that should be parsed
-	return transcripts[:10]
+# takes string of ingredients and/or recipes
+
+# use both or have different modules to see which one is better
+# return the resultant recipe list json that should be parsed
+
+# if recipe(s) given compute similarity of recipe names using edit distance
+    if r != '':
+        rec_list = r.split(",")
+        recipes_r = [r for l in [find_similar(rec) for rec in rec_list] for r in l]
+        recipes_r = [r[1] for r in sorted(recipes_r, key=lambda tup: tup[0])]
+
+# if ingredient(s) given compute intersection of output of recipes from inverted index      
+    if i != '':
+        ing_list = i.split(",")
+        rec_ind = []
+        for ing in ing_list:
+            if ing in inverted_index:
+                indices = inverted_index[ing]
+                rec_ind.append(indices)
+        common_indices = set.intersection(*map(set,rec_ind))
+        recipes_i = [transcripts[i] for i in common_indices]
+
+ # if both recipes and ingredients given, return intersection list of recipes sorted in order of edit distance     
+    if r !='' and i !='':
+        ranked_recipes = [r for r in recipes_r for i in recipes_i if i['code']==r['code']]
+      
+    if r == '':
+        ranked_recipes = recipes_i
+    
+    else:
+        ranked_recipes = recipes_r
+   
+    return ranked_recipes
