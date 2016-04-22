b6be32ef201436a4f3d8b729ebcb0d53cb95d798
danieldouvris <daniel.douvris@gmail.com>
2016-04-22 13:41:45 -0400

    update test to use index search in find recipes

--- project_template/test.py
+++ project_template/test.py
@@ -2,6 +2,7 @@ from models import Docs
 import os
 import Levenshtein
 import json
+from .similarity import *
 
 
 def read_file(n):
@@ -37,34 +38,36 @@ def find_similar(q):
 def find_recipes(i,r=''):
 # takes string of ingredients and/or recipes
 
+    return index_search(i, inverted_index, idf, norms, recipes)
+
 # use both or have different modules to see which one is better
 # return the resultant recipe list json that should be parsed
 
 # if recipe(s) given compute similarity of recipe names using edit distance
-    if r != '':
-        rec_list = r.split(",")
-        recipes_r = [r for l in [find_similar(rec) for rec in rec_list] for r in l]
-        recipes_r = [r[1] for r in sorted(recipes_r, key=lambda tup: tup[0])]
+    # if r != '':
+    #     rec_list = r.split(",")
+    #     recipes_r = [r for l in [find_similar(rec) for rec in rec_list] for r in l]
+    #     recipes_r = [r[1] for r in sorted(recipes_r, key=lambda tup: tup[0])]
 
 # if ingredient(s) given compute intersection of output of recipes from inverted index      
-    if i != '':
-        ing_list = i.split(",")
-        rec_ind = []
-        for ing in ing_list:
-            if ing in inverted_index:
-                indices = inverted_index[ing]
-                rec_ind.append(indices)
-        common_indices = set.intersection(*map(set,rec_ind))
-        recipes_i = [transcripts[i] for i in common_indices]
+    # if i != '':
+    #     ing_list = i.split(",")
+    #     rec_ind = []
+    #     for ing in ing_list:
+    #         if ing in inverted_index:
+    #             indices = inverted_index[ing]
+    #             rec_ind.append(indices)
+    #     common_indices = set.intersection(*map(set,rec_ind))
+    #     recipes_i = [transcripts[i] for i in common_indices]
 
  # if both recipes and ingredients given, return intersection list of recipes sorted in order of edit distance     
-    if r !='' and i !='':
-        ranked_recipes = [r for r in recipes_r for i in recipes_i if i['code']==r['code']]
+    # if r !='' and i !='':
+    #     ranked_recipes = [r for r in recipes_r for i in recipes_i if i['code']==r['code']]
       
-    if r == '':
-        ranked_recipes = recipes_i
+    # if r == '':
+    #     ranked_recipes = recipes_i
     
-    else:
-        ranked_recipes = recipes_r
+    # else:
+    #     ranked_recipes = recipes_r
    
-    return ranked_recipes
+    # return ranked_recipes
