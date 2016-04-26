2fc3ea19d00c18290ccaabec45fa27b43c306739
sv387 <sv387@cornell.edu>
2016-04-25 21:16:43 -0400

    Account for the similarity of ing

--- project_template/similarity.py
+++ project_template/similarity.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import sparse, io
 import pickle
 from collections import defaultdict
+import Levenshtein
 
 ing_by_rec = io.mmread("data/ing_by_rec.mtx").tocsr().toarray()
 
@@ -19,6 +20,27 @@ with open("data/norm.npy", "rb") as f:
 
 n_ing = len(ing_to_index)
 
+
+def distanceMeasure(query, msg):
+    try:
+        query = str(query).lower()
+    except UnicodeEncodeError:
+        query = (query.encode('utf8')).lower()
+    
+    try:
+        msg = str(msg).lower()
+    except UnicodeEncodeError:
+        msg = (msg.encode('utf8')).lower()
+        
+    return Levenshtein.distance(query, msg)
+
+
+def findMostSimilar(i,ingList):
+    result = []
+    for ing in ingList:        
+        result.append(((distanceMeasure(i, ing)), ing))
+    return sorted(result, key=lambda tup: tup[0])
+
 def index_search(query, n_ing, ibr, idf, ing_to_index, norm, recipes):
     if query == "":
         return []
@@ -27,10 +49,11 @@ def index_search(query, n_ing, ibr, idf, ing_to_index, norm, recipes):
         q_vec = np.zeros([n_ing])
         query_toks = [q.strip() for q in query.split(",")]
         query_set = set(query_toks)
+        
         # Construct query vector
         for q in query_toks:
             if q in ing_to_index:
-                q_vec[ing_to_index[q]] = idf[ing_to_index[q]]
+                q_vec[ing_to_index[q]] = idf[ing_to_index[q]]        
         # Normalize query vector with l2 norm
         q_norm = (q_vec * q_vec).sum()**0.5
         q_vec /= q_norm
@@ -41,6 +64,13 @@ def index_search(query, n_ing, ibr, idf, ing_to_index, norm, recipes):
         order = sorted(enumerate(scores.flat), key=lambda pair:pair[1], reverse=True)
         results = [recipes[o[0]] for o in order if o[1] > 0]
         for rec in results:
-            rec['diff'] = ", ".join(list(set(rec['ing']) - query_set))
-            rec['match'] = ", ".join(list(set(rec['ing']) & query_set))
+            matchedIngs = set()
+            recIngList = list(set(rec['ing']))
+            for ing in query_set:
+                matchingIngs = [recipeIng for recipeIng in recIngList if ing in recipeIng.lower()]
+                if len(matchingIngs)>0:
+                    score,toUseIng = findMostSimilar(ing,matchingIngs)[0]
+                    matchedIngs.add(toUseIng)                
+            rec['diff'] = ", ".join(list(set(rec['ing']) - matchedIngs))
+            rec['match'] = ", ".join(list(set(rec['ing']) & matchedIngs))
         return results
