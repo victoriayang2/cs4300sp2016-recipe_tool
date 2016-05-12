6f463747a73786069fd5f1cd3fc01e964eab6600
danieldouvris <daniel.douvris@gmail.com>
2016-05-12 02:29:31 -0400

    wrong settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd7suj672n3qip',
+        'USER': 'fpaekdihbgosnx',
+        'PASSWORD': ENV['DBPASS'],
+        'HOST': 'ec2-50-17-197-76.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
