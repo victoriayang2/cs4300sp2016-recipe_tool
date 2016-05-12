b6dd5d1fc58072bfd2f2e6b1c2920d3ddc67df60
danieldouvris <daniel.douvris@gmail.com>
2016-05-12 02:32:41 -0400

    use dj_database_url

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,15 +76,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd7suj672n3qip',
-        'USER': 'fpaekdihbgosnx',
-        'PASSWORD': ENV['DBPASS'],
-        'HOST': 'ec2-50-17-197-76.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'd7suj672n3qip',
+    #     'USER': 'fpaekdihbgosnx',
+    #     'PASSWORD': ENV['DBPASS'],
+    #     'HOST': 'ec2-50-17-197-76.compute-1.amazonaws.com',
+    #     'PORT': '5432',
+    # }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
